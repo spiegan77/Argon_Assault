8bb43be64a6d90465e57c847b74a6311b7838a3b
Steve_U57 <obsidian77@gmail.com>
2021-02-07 07:55:53 -0500

    91_Unitys_Scripts

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // This is required for loading scenes
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject); //gameObject is the "thing" the script is attached to
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f); // Load first scene after 2 seconds
+    }
+        void LoadFirstScene()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+}
